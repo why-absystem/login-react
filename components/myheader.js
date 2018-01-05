cff7ff78204f4050e870f64f9ed0923745d582ae
Wahyu Hidayat <hidayatwahyu87@gmail.com>
2018-01-05 19:19:31 +0700

    buat component

---
+++ components/myheader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Myheader extends Component {
+	render() {
+		return (
+			<div>
+				<p>
+					Myheader
+				</p>
+			</div>
+		);
+	}
+}
