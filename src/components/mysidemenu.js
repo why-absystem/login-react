d2b7754ee221bfefb4232e3855218fae1db04674
Wahyu Hidayat <hidayatwahyu87@gmail.com>
2018-01-05 19:24:40 +0700

    buat sidemenu

---
+++ src/components/mysidemenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Mysidemenu extends Component {
+	render() {
+		return (
+			<div>
+				<p>
+					Mysidemenu
+				</p>
+			</div>
+		);
+	}
+}
